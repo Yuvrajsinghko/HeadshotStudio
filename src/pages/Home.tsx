561b1167a43a400ab30be860ea1eee3d385c9eca
Yuvraj Singh <yuvrajprince19@gmail.com>
2026-06-19 21:11:18 +0530

    updated some files

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,22 @@
 import { Hero } from "../components/Hero";
+import UploadCard from "../components/UploadCard";
+import { useHeadshot } from "../hooks/useHeadshot";
 
 const Home = () => {
+  const { uploadError, uploadStatus, handleUploadError, handleUploadStart } =
+    useHeadshot();
   return (
     <div className="min-h-screen">
       <header className="border-b border-white/10 px-4 py-4">
         <div className="text-lg font-bold">
-          <span><span className="text-indigo-400">HeadShot</span> Studio</span>
+          <span>
+            <span className="text-indigo-400">HeadShot</span> Studio
+          </span>
         </div>
       </header>
+
       <Hero />
+      <UploadCard uploadStatus={uploadStatus} uploadError={uploadError} onUploadError = {handleUploadError} onUploadStart = {handleUploadStart} />
     </div>
   );
 };
