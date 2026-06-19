4ec323cd9af8c48968c84f457a520c2a509033c8
Yuvraj Singh <yuvrajprince19@gmail.com>
2026-06-19 21:09:41 +0530

    started implementing upload card component

---
+++ src/components/UploadCard.tsx
@@ -0,0 +1,49 @@
+import { ImageIcon } from "lucide-react";
+import type { UploadStatus } from "../types";
+
+interface UploadCardProps {
+  uploadStatus: UploadStatus;
+  uploadError: string | null;
+  onUploadError: (error: Error) => void;
+  onUploadStart: () => void;
+}
+const UploadCard = ({
+  uploadError,
+  uploadStatus,
+  handleUploadError,
+  handleUploadStart,
+}: UploadCardProps) => {
+  const uploadFile = async (file: File) => {
+    try {
+      console.log("hehe");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <section id="upload" className="px-4 py-12">
+      <div className="mx-auto mx-w-2xl">
+        <h2 className="mb-2 text-center text-2xl font-semibold">
+          Upload Your Selfie
+        </h2>
+        <p className="mb-8 text-center text-white/60">
+          Drag, drop, or click to upload your photo
+        </p>
+        <div className="glass-card relative flex cursor-pointer flex-col items-center gap-6 p-10 transition">
+          <input />
+          <div className="flex h-20 w-20 items-center justify-center rounded-2xl bg-indigo-500/20 text-indigo-400">
+            <ImageIcon className="h-10 w-10" />
+          </div>
+          <div className="text-center">
+            <p className="text-lg font-medium">Drag & drop your selfie here</p>
+            <p className="mt-1 text-sm text-white/50">
+              or click to browse · JPG, PNG, OR WEBP
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default UploadCard;
