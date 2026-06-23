b5e90f04601e524e8d366d8c2063285eac55ba50
Yuvraj Singh <yuvrajprince19@gmail.com>
2026-06-23 12:36:23 +0530

    feat:implement drag and drop functionality

--- src/components/UploadCard.tsx
+++ src/components/UploadCard.tsx
@@ -1,5 +1,6 @@
 import { ImageIcon } from "lucide-react";
 import type { UploadStatus } from "../types";
+import { useDropzone } from "react-dropzone";
 
 interface UploadCardProps {
   uploadStatus: UploadStatus;
@@ -7,11 +8,18 @@ interface UploadCardProps {
   onUploadError: (error: Error) => void;
   onUploadStart: () => void;
 }
+
+const ACCEPT = {
+  "image/jpeg": [".jpg", ".jpeg"],
+  "image/png": [".png"],
+  "image/webp": [".webp"],
+};
 const UploadCard = ({
-  uploadError,
   uploadStatus,
-  handleUploadError,
-  handleUploadStart,
+  uploadError,
+  onUploadError,
+  onUploadStart,
+  onUploadSuccess,
 }: UploadCardProps) => {
   const uploadFile = async (file: File) => {
     try {
@@ -19,7 +27,24 @@ const UploadCard = ({
     } catch (error) {
       console.log(error);
     }
+  }
+  const onDrop = (acceptedFiles:File[])=>{
+    if(acceptedFiles.length ===0){
+      onUploadError(new Error ("Please upload a JPG,PNG,or WEBP image."));
+      return;
+    }
+    console.log(acceptedFiles);
+
+  
   };
+  const { getRootProps, getInputProps, isDragActive, open } = useDropzone({
+    onDrop,
+    accept: ACCEPT,
+    maxFiles: 1,
+    multiple: false,
+    disabled: uploadStatus === "uploading",
+  });
+
   return (
     <section id="upload" className="px-4 py-12">
       <div className="mx-auto mx-w-2xl">
@@ -29,8 +54,8 @@ const UploadCard = ({
         <p className="mb-8 text-center text-white/60">
           Drag, drop, or click to upload your photo
         </p>
-        <div className="glass-card relative flex cursor-pointer flex-col items-center gap-6 p-10 transition">
-          <input />
+        <div {...getRootProps()} className="glass-card relative flex cursor-pointer flex-col items-center gap-6 p-10 transition">
+          <input {...getInputProps()} />
           <div className="flex h-20 w-20 items-center justify-center rounded-2xl bg-indigo-500/20 text-indigo-400">
             <ImageIcon className="h-10 w-10" />
           </div>
